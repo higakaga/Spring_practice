a5b39f5ead6249046edccbacbfdf92da7e7e7ea8
higakaga <higakaga@gmail.com>
2024-12-24 23:10:05 +0900

    spring_start

--- spring_start/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ spring_start/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,6 +25,17 @@ public class MemberService {
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+//        long start = System.currentTimeMillis();
+//        try{
+//            // 같은 이름이 있는 중복 회원 X
+//            validateDuplicateMember(member); // 중복 회원 검증
+//            memberRepository.save(member);
+//            return member.getId();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("join = " + timeMs + "ms");
+//        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -35,6 +48,14 @@ public class MemberService {
     // 전체 회원 조회
     public List<Member> findMembers() {
         return memberRepository.findAll();
+//        long start = System.currentTimeMillis();
+//        try{
+//            return memberRepository.findAll();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("findMembers = " + timeMs + "ms");
+//        }
     }
 
     public Optional<Member> findOne(Long memberId) {
